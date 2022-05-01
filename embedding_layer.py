72cae69777b362d88e11a5f0db6e2c5c3bb0a18c
Zhang guangyin <aauiuui@163.com>
2022-05-01 22:24:33 +0800

    second commit

--- embedding_layer.py
+++ embedding_layer.py
@@ -3,5 +3,216 @@
 Created on Fri Apr 29 09:58:47 2022
 
 @author: 1222
+tf.nn.embedding_lookup
+tf.gather
+两种实现embedding查表的方式
+
 """
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras import initializers
+from tensorflow.keras import activations
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Layer
+ 
+class Myembeddinglayer(Layer):
+    
+    def __init__(self,T2Tattentionwidth,positionembeddinglength,kernel_initializer='glorot_uniform',
+                 pretrainedtextweight=None,hiddensize=768,**kwargs):
+        if output_dim==None:
+            self.output_dim=hiddensize
+        else:
+            self.output_dim=output_dim
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.hiddensize=hiddensize
+        super(MySelfAttention,self).__init__(**kwargs)
+        self.supports_masking = True
+        self.local_radius=local_radius
+        self.pretrainedtextweight=pretrainedtextweight
+        self.supports_masking = True
+        self.positionembeddinglength=positionembeddinglength#用来计算相对位置编码的长度
+        self.T2Tattentionwidth=T2Tattentionwidth
+        
+        
+    def build(self,input_shape):
+        self.w_html=self.add_weight(name='W_V_F',
+             shape=(self.hiddensize,self.hiddensize),
+             initializer=self.kernel_initializer,
+             trainable=True)
+        self.w_text=self.add_weight(name='W_V_F',
+             shape=(self.hiddensize,self.hiddensize),
+             initializer=self.my_init,
+             trainable=True)
+        self.position_embedding=self.add_weight(name='relative_position_embedding',
+             shape=(self.positionembeddinglength,self.hiddensize),
+             initializer=self.my_init,
+             trainable=True)
+        self.htmledge_embedding=self.add_weight(name='htmledge_embedding',
+             shape=(3,self.hiddensize),
+             initializer=self.my_init,
+             trainable=True)
+
+    def call(self,inputs,**kwargs):
+        fieldlist,textlist,htmllist,sequencelist=inputs[0],inputs[1],inputs[2],inputs[4]
+        textpadlength=textlist.shape[1]
+        htmlsequencelength=htmllist.shape[1]
+        field_embeds = tf.gather(params=self.w_text, indices=field_list)
+        text_embeds = tf.gather(params=self.w_text, indices=text_list)
+        html_embeds = tf.gather(params=self.w_html, indices=html_list)
+        T2Tmask=self.computeT2Tmask(sequencelist,textpadlength)
+        H2Hmask=self.computeH2Hmask(inputs[3][0],htmlsequencelength)#inputs[3]包含两部分，一部分是html edge list,一部分是inner text list
+        H2Tmask=self.computeH2Tmask(inputs[3][1],textpadlength,htmlsequencelength)
+        T2Hmask=self.computeT2Hmask(htmllist)
+        htmledge_embedding_complete=tf.concat((self.htmledge_embedding,K.ones((1,self.hiddensize))),axis=0)
+        return [field_embeds,text_embeds,html_embeds,T2Tmask,H2Hmask,H2Tmask,T2Hmask,self.position_embedding,htmledge_embedding_complete]
+
+
+    def computeT2Tmask(self,sequencelist,textpadlength):
+        #计算T2T mask矩阵，这个其实也分两步，只不过其中一步可以直接计算
+#        sequencelist=[[-1,5,9,20,50,53,59],#这个最后一个数字不必是textlist的总长度
+#                    [-1,24,34,50,59]]#text pad后的总长度是60
+        onesmatrix=np.ones((len(sequencelist),textpadlength,textpadlength))#batchsize,textpadlength,textpadlength
+        attention_width=self.T2Tattentionwidth
+        for i in range(len(sequencelist)):
+            for j in range(1,len(sequencelist[i])):
+                length=sequencelist[i][j]-sequencelist[i][j-1]
+                raw=np.zeros((length,length))
+                for k in range(length):
+                    for l in range(length):
+                        if k-l>attention_width or l-k>attention_width:
+                            raw[k][l]=1
+                onesmatrix[i,(sequencelist[i][j-1]+1):(sequencelist[i][j]+1),(sequencelist[i][j-1]+1):(sequencelist[i][j]+1)]=raw
+        return tf.convert_to_tensor(onesmatrix,dtype=tf.float32)
+    
+    
+    def computeH2Hmask(self,html_edge_list,htmlsequencelength):
+        ##计算H2H mask矩阵，这个还分为2步
+        htmledgematrix=np.ones((len(html_edge_list),htmlsequencelength,htmlsequencelength))#batchsize,htmlpadlength,htmlpadlength
+        #shape=np.shape(test)
+        for i in range(len(html_edge_list)):
+            for j in range(len(html_edge_list[i])):
+                htmledgematrix[i][html_edge_list[i][j][0]][html_edge_list[i][j][2]]=html_edge_list[i][j][1]
+        #计算H2H mask矩阵
+        #equal = tf.equal(htmledgematrix, threshold)
+        #c=tf.cast(c,tf.float32)
+        return tf.convert_to_tensor(htmledgematrix,dtype=tf.int64)
+
+
+    def computeH2Tmask(self,html_text_edge_list,textpadlength,htmlsequencelength):
+        #计算H2T mask矩阵
+        #html_text_edge_list(html_index,text_index)
+        H2Tmask=np.ones((len(html_text_edge_list),60,70))#batchsize,htmllength,textpadlength
+        for i in range(len(html_text_edge_list)):
+            for j in range(len(html_text_edge_list[i])):
+                H2Tmask[i][html_edge_list[i][j][0]][html_edge_list[i][j][1]]=0
+        return tf.convert_to_tensor(H2Tmask,dtype=tf.float32)
+    
+    def computeT2Hmask(self,htmllist):
+        #计算T2H mask矩阵
+        #mask=K.ones((16,32))
+        #e=K.ones((16,40,32))
+        T2Hmask=tf.equal(tf.convert_to_tensor(htmllist,dtype=tf.int32), 0)#这个0是pad字符的index
+        return T2Hmask#batchsize,htmllistlength
+#        mask = K.expand_dims(K.cast(mask, K.floatx()), axis=1)#expand_dims增加向量维度，就是增加一个维度为一的维度
+#        e=e-10000*mask
+
+
+
+    # def my_init(self,**kwargs):
+    #     return self.pretrainedtextweight#K.random_normal(shape, dtype=dtype)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class TFBertEmbeddings(tf.keras.layers.Layer):
+    """Construct the embeddings from word, position and token_type embeddings."""
+
+    def __init__(self, config: BertConfig, **kwargs):
+        super().__init__(**kwargs)
+
+        self.vocab_size = config.vocab_size
+        self.type_vocab_size = config.type_vocab_size
+        self.hidden_size = config.hidden_size
+        self.max_position_embeddings = config.max_position_embeddings
+        self.initializer_range = config.initializer_range
+        self.LayerNorm = tf.keras.layers.LayerNormalization(epsilon=config.layer_norm_eps, name="LayerNorm")
+        self.dropout = tf.keras.layers.Dropout(rate=config.hidden_dropout_prob)
+
+    def build(self, input_shape: tf.TensorShape):
+        with tf.name_scope("word_embeddings"):
+            self.weight = self.add_weight(
+                name="weight",
+                shape=[self.vocab_size, self.hidden_size],
+                initializer=get_initializer(self.initializer_range),
+            )
+
+        with tf.name_scope("token_type_embeddings"):
+            self.token_type_embeddings = self.add_weight(
+                name="embeddings",
+                shape=[self.type_vocab_size, self.hidden_size],
+                initializer=get_initializer(self.initializer_range),
+            )
+
+        with tf.name_scope("position_embeddings"):
+            self.position_embeddings = self.add_weight(
+                name="embeddings",
+                shape=[self.max_position_embeddings, self.hidden_size],
+                initializer=get_initializer(self.initializer_range),
+            )
+
+        super().build(input_shape)
+
+    def call(
+        self,
+        input_ids: tf.Tensor = None,
+        position_ids: tf.Tensor = None,
+        token_type_ids: tf.Tensor = None,
+        inputs_embeds: tf.Tensor = None,
+        past_key_values_length=0,
+        training: bool = False,
+    ) -> tf.Tensor:
+        """
+        Applies embedding based on inputs tensor.
+        Returns:
+            final_embeddings (`tf.Tensor`): output embedding tensor.
+        """
+        if input_ids is None and inputs_embeds is None:
+            raise ValueError("Need to provide either `input_ids` or `input_embeds`.")
+
+        if input_ids is not None:
+            inputs_embeds = tf.gather(params=self.weight, indices=input_ids)
+
+        input_shape = shape_list(inputs_embeds)[:-1]
+
+        if token_type_ids is None:
+            token_type_ids = tf.fill(dims=input_shape, value=0)
+
+        if position_ids is None:
+            position_ids = tf.expand_dims(
+                tf.range(start=past_key_values_length, limit=input_shape[1] + past_key_values_length), axis=0
+            )
+
+        position_embeds = tf.gather(params=self.position_embeddings, indices=position_ids)
+        token_type_embeds = tf.gather(params=self.token_type_embeddings, indices=token_type_ids)
+        final_embeddings = inputs_embeds + position_embeds + token_type_embeds
+        final_embeddings = self.LayerNorm(inputs=final_embeddings)
+        final_embeddings = self.dropout(inputs=final_embeddings, training=training)
+
+        return final_embeddings
+
 
--- seqselfattention.py
+++ seqselfattention.py
@@ -10,9 +10,9 @@ from tensorflow.keras import activations
 from tensorflow.keras import backend as K
 from tensorflow.keras.layers import Layer
  
-class MySelfAttention(Layer):
+class webformerAttention(Layer):
     
-    def __init__(self,local_radius=None,output_dim=None,kernel_initializer='glorot_uniform',hiddensize=768,**kwargs):
+    def __init__(self,maxposition=None,output_dim=None,kernel_initializer='glorot_uniform',hiddensize=768,**kwargs):
         if output_dim==None:
             self.output_dim=hiddensize
         else:
@@ -21,7 +21,8 @@ class MySelfAttention(Layer):
         self.hiddensize=hiddensize
         super(MySelfAttention,self).__init__(**kwargs)
         self.supports_masking = True
-        self.local_radius=local_radius
+        self.max_position=maxposition
+        
         
     def build(self,input_shape):#input_shape((batchsize,sequencelength,hidden size))
         # self.W=self.add_weight(name='W',
@@ -74,15 +75,26 @@ class MySelfAttention(Layer):
              trainable=True)
         self.built = True
         
-    def call(self,inputs, mask=None, **kwargs):
+    def call(self,inputs,**kwargs):
+
+        field, text, html,textsequencematrix,positionembedding= inputs[0], inputs[1], inputs[2], inputs[-1]
+        H2Hmask=inputs[4]
+        htmledgeembedding=inputs[-1]#batchsize htmllength htmllength
+        H2Tmask=inputs[5]
+        T2Tmask=inputs[3]
+        T2Hmask=inputs[6]
+        textlength=text.shape[1]
         
-        field, html, text = inputs[0], inputs[1], inputs[2]
+        
         text_v=K.dot(text,self.W_V_T)
         html_v=K.dot(html,self.W_V_H)
         #H2H attention
         H2H_q=K.dot(html,self.W_Q_H2H)#x(batchsize,sequencelength,hidden size)(hiddensize, hiddensize)(batchsize,sequencelength,hidden size)
-        H2H_k=K.dot(html,self.W_K_H2H)
-        H2H_e=K.batch_dot(H2H_q,K.permute_dimensions(H2H_k,[0,2,1]))#把k转置，并与q点乘,dot的维度长度必须相同#(batchsize,sequencelength,hidden size)(batchsize,hidden size,sequencelength)(batchsize,sequencelength,sequencelength)
+        #H2H_k=K.dot(html,self.W_K_H2H)
+        H2H_k=tf.add(tf.expand_dims(K.dot(html,self.W_K_H2H),1),K.gather(htmledgeembedding, H2Hmask))
+        #batchsize htmllength hiddensize
+        #H2H_e=K.batch_dot(H2H_q,K.permute_dimensions(H2H_k,[0,2,1]))#
+        H2H_e=tf.reduce_sum(tf.multiply(tf.expand_dims(H2H_q,1),H2H_k),axis=-1)
         #H2T attention
         H2T_q=K.dot(html,self.W_Q_H2T)#x(batchsize,sequencelength,hidden size)(hiddensize, hiddensize)(batchsize,sequencelength,hidden size)
         H2T_k=K.dot(text,self.W_K_H2T)
@@ -93,8 +105,10 @@ class MySelfAttention(Layer):
         T2H_e=K.batch_dot(T2H_q,K.permute_dimensions(T2H_k,[0,2,1]))#把k转置，并与q点乘,dot的维度长度必须相同
         #T2T attention
         T2T_q=K.dot(text,self.W_Q_T2T)#x(batchsize,sequencelength,hidden size)(hiddensize, hiddensize)(batchsize,sequencelength,hidden size)
-        T2T_k=K.dot(text,self.W_K_T2T)
-        T2T_e=K.batch_dot(T2T_q,K.permute_dimensions(T2T_k,[0,2,1]))#把k转置，并与q点乘,dot的维度长度必须相同
+        pos_ids=self.cal_rel_pos_matri(textlength)
+        T2T_k=tf.add(tf.expand_dims(K.dot(text,self.W_K_T2T),1),tf.expand_dims(K.gather(positionembeddings, pos_ids), 0))
+        T2T_e=tf.reduce_sum(tf.multiply(tf.expand_dims(T2T_q,1),T2T_k),axis=-1)
+        #T2T_e=K.batch_dot(T2T_q,K.permute_dimensions(T2T_k,[0,2,1]))#把k转置，并与q点乘,dot的维度长度必须相同
         
         """
         batch_dot
@@ -108,30 +122,38 @@ class MySelfAttention(Layer):
         H2T_e=H2T_e/(self.output_dim**0.5)
         T2H_e=T2H_e/(self.output_dim**0.5)
         T2T_e=T2T_e/(self.output_dim**0.5)
-        #mask[0]H2H mask
-        #mask[1]H2T mask
-        #mask[2]T2T mask
-        H2H_e-=10000(1-K.cast(mask[0], K.floatx())
-        H2T_e-=10000(1-K.cast(mask[1], K.floatx())
-        T2T_e-=10000(1-K.cast(mask[2], K.floatx())
+
+        H2Hmask_2=tf.equal(H2Hmask,0)#0代表两个tag之间没有边连接
+        H2Hmask_2=tf.cast(H2Hmask_2,tf.float32)
         
+        #H2H_e-=10000(1-K.cast(mask[0], K.floatx())
+        H2H_e-=10000*H2Hmask_2#(batchsize,htmllength,textlength)
         
+        #H2T_e-=10000(1-K.cast(mask[1], K.floatx())
+        H2T_e-=10000*H2Tmask#(batchsize,htmllength,textlength)
+        #T2T_e-=10000(1-K.cast(mask[2], K.floatx())
+        T2T_e-=10000*T2Tmask#textsequencematrix(batchsize,textlength,textlength)
+
+
         H2H_e=K.softmax(H2H_e)
 #        print(K.int_shape(H2H_e))
         html1=K.batch_dot(H2H_e,html_v)
-        
+
         H2T_e=K.softmax(H2T_e)
         html2=K.batch_dot(H2T_e,text_v)
-        
+
         T2H_e=K.softmax(T2H_e)
         text1=K.batch_dot(T2H_e,html_v)
-                
+
         T2T_e=K.softmax(T2T_e)
         text2=K.batch_dot(T2T_e,text_v)
         
         html=html1+html2
         text=text1+text2
+        return [field,text,html,inputs[3],inputs[4],inputs[5],inputs[6],inputs[7],inputs[8],inputs[9]]
     
+
+
     def compute_mask(self, inputs, mask=None):
         if isinstance(mask, list):
             mask = mask[0]
@@ -142,10 +164,112 @@ class MySelfAttention(Layer):
     def compute_output_shape(self,input_shape):
         return (input_shape[0],input_shape[1],self.output_dim)
 
+    def cal_rel_pos_matri(self,textlength):
+        # batchsequencelist=inputs[1],inputs[4]#数据以batch的形式输入
+        # batchsize,textlength=np.shape(batchtextlist)
+        # textrealtiveposition=tf.zeros(batchsize,textlength,textlength)
+        # for sample in batchsequencelist:
+        #     for j in sample:
+        #         tf.zeros
+
+        # 计算相对位置矩阵位置差
+        # 一维[0,1,...,q_seq_len]
+        q_idxs = K.arange(0, textlength, dtype='int32')
+        # [[0] [1] [2] [3] ... [q_seq_len]]
+        q_idxs = K.expand_dims(q_idxs, 1)
+        v_idxs = K.arange(0, textlength, dtype='int32')
+        # [[0,1,...,v_seq_len]]
+        v_idxs = K.expand_dims(v_idxs, 0)
+        pos_ids = v_idxs - q_idxs
+        '''以q_seq_len=v_seq_len=9为例：
+         [[ 0  1  2  3  4  5  6  7  8  9]
+          [-1  0  1  2  3  4  5  6  7  8]
+          [-2 -1  0  1  2  3  4  5  6  7]
+          [-3 -2 -1  0  1  2  3  4  5  6]
+          [-4 -3 -2 -1  0  1  2  3  4  5]
+          [-5 -4 -3 -2 -1  0  1  2  3  4]
+          [-6 -5 -4 -3 -2 -1  0  1  2  3]
+          [-7 -6 -5 -4 -3 -2 -1  0  1  2]
+          [-8 -7 -6 -5 -4 -3 -2 -1  0  1]
+          [-9 -8 -7 -6 -5 -4 -3 -2 -1  0]]
+          相对位置编码就比较简单的用这种差几位数来表示相对位置
+        '''
+        # 后处理操作
+        #max_position =self.maxposition #(self.input_dim - 1) // 2
+        '''
+        K.clip：逐元素clip，将pos_ids中超出(-max_position, max_position)范围的数强制变为边界值
+        1、作者假设精确的相对位置编码在超出了一定距离之后是没有必要的
+        2、截断最大距离使得模型的泛化效果好，可以更好的generalize到没有在训练阶段出现过的序列长度上
+        比如上面的例子中, 截到(-4,4)之间为：
+        [[ 0  1  2  3  4  4  4  4  4  4]
+         [-1  0  1  2  3  4  4  4  4  4]
+         [-2 -1  0  1  2  3  4  4  4  4]
+         [-3 -2 -1  0  1  2  3  4  4  4]
+         [-4 -3 -2 -1  0  1  2  3  4  4]
+         [-4 -4 -3 -2 -1  0  1  2  3  4]
+         [-4 -4 -4 -3 -2 -1  0  1  2  3]
+         [-4 -4 -4 -4 -3 -2 -1  0  1  2]
+         [-4 -4 -4 -4 -4 -3 -2 -1  0  1]
+         [-4 -4 -4 -4 -4 -4 -3 -2 -1  0]]
+        '''
+        pos_ids = K.clip(pos_ids, -self.max_position,
+                         self.max_position)
+        pos_ids = pos_ids + self.max_position  # shape=(q_seq_lenv, v_seq_len)
+        return pos_ids
 
 
-html = tf.ones((32,500,768))
-text=tf.ones((32,600,768))
+
+
+
+
+
+
+
+
+
+text=K.ones((16,500,768))
+html=K.ones((500,500,768))
+text=K.random_normal((4,10,20))
+html=K.random_normal((10,10,20))
+html = tf.expand_dims(html, 0)
+text = tf.expand_dims(text, 1)
+result=tf.add(text,html)
+result[0,0,1,:]==text[0,0,1,:]+html[0,0,1,:]
+
+
+
+test=tf.convert_to_tensor([[1,1,1,1,1,0,0,0,0],
+                      [1,1,1,1,1,1,1,0,0],
+                      [1,1,1,1,1,0,0,0,0]])
+c=tf.cast(tf.equal(test, 0),tf.float32)
+
+
+
+
+
+another=K.placeholder((16,1,500,768))
+another2=K.placeholder((16,500,500,768))
+
+another=K.ones((16,500,768))
+another = tf.expand_dims(another, 2)
 field=tf.ones((32,5,768))
 testattention=MySelfAttention()
 testattention(inputs=[field,html,text])
+K.shape(tf.reduce_sum(tf.multiply(another,another2,),axis=-1)#tf.squeeze(axes=(-1,-1)
+K.shape(tf.einsum("mnp,onp->mon",text,html))
+result=tf.einsum("mnp,onp->mon",text,html)
+
+result[0,0,0]==K.dot(text[0,0,:],html[0,0,:])
+
+
+
+test1=K.placeholder((3,768))
+test2=K.placeholder((1,768))
+test3=K.placeholder((16,500,500),dtype="int32")
+test4=tf.add(tf.expand_dims(test1,1),K.gather(test2, test3))
+test5=K.placeholder((16,500,768))
+tf.reduce_sum(tf.multiply(tf.expand_dims(test5,1),test4),axis=-1)
+
+
+
+tf.concat([test1,test2], axis=0, name='concat')：
